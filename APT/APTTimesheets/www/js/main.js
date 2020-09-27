24c2ebc780b63f027bac2e36d9b9f0cc1be41105
= <=>
2020-09-27 18:45:19 +0100

    new upload

--- APT/APTTimesheets/www/js/main.js
+++ APT/APTTimesheets/www/js/main.js
@@ -4,8 +4,8 @@ let i = 2;
 document.getElementById('btn-add-new-planned-synthetic').onclick = function () {
 
     let template = `
-                Synthetic <input type="text" name="plannedsynthetic[${i}]['plannedsynthetic']" value="<?php echo $plannedSynthetics[${i}][0]; ?>">
-                Quantity <input type="text" name="plannedsynthetic[${i}]['quantity']" value="<?php echo $plannedSynthetics[${i}][1]; ?>"><br>
+                Synthetic <input type="text" name="plannedsynthetic[${i}]['plannedsynthetic']">
+                Quantity <input type="text" name="plannedsynthetic[${i}]['quantity']"><br>
     `;
 
     let container = document.getElementById('planned-synthetics-container');
@@ -21,9 +21,9 @@ let j = 2;
 document.getElementById('btn-add-new-unplanned-synthetic').onclick = function () {
 
     let template = `
-            Synthetic <input type="text" name="unplannedsynthetic[${i}]['unplannedsynthetic']" value="<?php echo $unplannedSynthetics[${i-1}][0]; ?>">
-            Quantity <input type="text" name="unplannedsynthetic[${i}]['quantity']" value="<?php echo $unplannedSynthetics[${i-1}][1]; ?>">
-            <textarea id="textarea-unplanned-work-comments-box" name="unplannedsynthetic[${i-1}]['comments']" placeholder="Comments"><?php echo $unplannedSynthetics[0][2]; ?></textarea>
+            Synthetic <input type="text" name="unplannedsynthetic[${i}]['unplannedsynthetic']">
+            Quantity <input type="text" name="unplannedsynthetic[${i}]['quantity']">
+            <textarea id="textarea-unplanned-work-comments-box" name="unplannedsynthetic[${i-1}]['comments']"></textarea>
             <br>
     `;
 
