464b446fc0fefb60e6a92181addc34374900874c
= <=>
2020-09-30 16:17:10 +0100

    new upload

--- APT/APTTimesheets/componenttests/attachments/upload.php
+++ APT/APTTimesheets/componenttests/attachments/upload.php
@@ -2,7 +2,7 @@
 
 
 
-$target_dir = "/uploads";
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -38,15 +38,17 @@ if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg
     $uploadOk = 0;
 }
 
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
+print_r($_FILES['fileToUpload']);
+
+// // Check if $uploadOk is set to 0 by an error
+// if ($uploadOk == 0) {
+//     echo "Sorry, your file was not uploaded.";
+// // if everything is ok, try to upload file
+// } else {
+//     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+//         echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+//     } else {
+//         echo "Sorry, there was an error uploading your file.";
+//     }
+// }
 ?>
