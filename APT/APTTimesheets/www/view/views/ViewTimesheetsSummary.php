a92d6ff2a6165c2d1ca90d3528feea857f58461f
= <=>
2020-10-02 16:32:34 +0100

    new upload

--- APT/APTTimesheets/www/view/views/ViewTimesheetsSummary.php
+++ APT/APTTimesheets/www/view/views/ViewTimesheetsSummary.php
@@ -4,33 +4,3 @@ namespace Phppot;
 require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/TimesheetSummaryRenderView.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/Timesheet.php';
 
-
-//if (session_status() == PHP_SESSION_NONE) {
-//    session_start();
-//}
-//
-//if(empty($_SESSION["userId"])) {
-//    echo "session userid empty";
-//    //Header('Location: ./loginFormView.php');
-//} else {
-//    //echo 'userid: ' . $_SESSION["userId"];
-//}
-//
-//
-//use http\Header;
-//use \Phppot\Member;
-
-
-//echo "Timesheet summary page !"; ?>
-
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!--<head>-->
-<!---->
-<!--    --><?php //require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/view/elements/ElementHeadTagElements.php'; ?>
-<!--    <head/>-->
-<!---->
-<!--<body>-->
-<?php //require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/view/elements/ElementMainMenu.php'; ?>
-<!--</body>-->
-
