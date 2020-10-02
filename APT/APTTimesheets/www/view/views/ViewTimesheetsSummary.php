f9dfe3888b36f0444af73c5d0220dc1c1064fa9a
= <=>
2020-10-02 16:37:34 +0100

    new upload

--- APT/APTTimesheets/www/view/views/ViewTimesheetsSummary.php
+++ APT/APTTimesheets/www/view/views/ViewTimesheetsSummary.php
@@ -1,15 +1,15 @@
 <?php
 namespace Phppot;
 
-echo 'working';
-echo getcwd();
-//if(include_once($_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/TimesheetSummaryRenderView.php')) {
-//    echo 'required file imported';
-//} else {
-//    echo 'required file error';
-//}
-echo '<br>';
-echo $_SERVER['DOCUMENT_ROOT'];
+//echo 'working';
+//echo getcwd();
+////if(include_once($_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/TimesheetSummaryRenderView.php')) {
+////    echo 'required file imported';
+////} else {
+////    echo 'required file error';
+////}
+//echo '<br>';
+//echo $_SERVER['DOCUMENT_ROOT'];
 
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/TimesheetSummaryRenderView.php';
