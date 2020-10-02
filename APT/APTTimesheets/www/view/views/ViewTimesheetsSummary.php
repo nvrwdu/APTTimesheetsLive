9b35d4244238a18c5cd555d8d5fb8580b96e2d95
= <=>
2020-10-02 12:53:40 +0100

    new upload

--- APT/APTTimesheets/www/view/views/ViewTimesheetsSummary.php
+++ APT/APTTimesheets/www/view/views/ViewTimesheetsSummary.php
@@ -4,62 +4,62 @@ namespace Phppot;
 require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/TimesheetSummaryRenderView.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/class/Timesheet.php';
 
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-
-if(empty($_SESSION["userId"])) {
-    echo "session userid empty";
-    //Header('Location: ./loginFormView.php');
-} else {
-    //echo 'userid: ' . $_SESSION["userId"];
-}
-
-
-use http\Header;
-use \Phppot\Member;
-
-
-//echo "Timesheet summary page !"; ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/view/elements/ElementHeadTagElements.php'; ?>
-<head/>
-
-<body>
-    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/view/elements/ElementMainMenu.php'; ?>
-</body>
-
-
-
-
+//if (session_status() == PHP_SESSION_NONE) {
+//    session_start();
+//}
+//
+//if(empty($_SESSION["userId"])) {
+//    echo "session userid empty";
+//    //Header('Location: ./loginFormView.php');
+//} else {
+//    //echo 'userid: ' . $_SESSION["userId"];
+//}
+//
+//
+//use http\Header;
+//use \Phppot\Member;
+//
+//
+////echo "Timesheet summary page !"; ?>
+<!---->
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--<head>-->
+<!---->
+<!--    --><?php //require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/view/elements/ElementHeadTagElements.php'; ?>
+<!--<head/>-->
+<!---->
+<!--<body>-->
+<!--    --><?php //require_once $_SERVER['DOCUMENT_ROOT'] . '/APT/APTTimesheets/www/view/elements/ElementMainMenu.php'; ?>
+<!--</body>-->
+<!---->
+<!---->
+<!---->
+<!---->
 <?php
-    // Get timesheet data for user
-    $timesheetData = new Timesheet();
-
-
-    switch ($_SESSION['userType']) {
-        case 'submitter' :
-            $timesheetData = $timesheetData->getTimesheetsByUserId($_SESSION['userId']);
-            //print_r($timesheetData);
-            //Pass timesheet data to renderer
-            $timesheetSummaryRenderView = new TimesheetSummaryRenderView($timesheetData);
-            $timesheetSummaryRenderView->render();
-            break;
-        case 'admin' :
-            $timesheetData = $timesheetData->getAssociatedUsersTimesheets($_SESSION['userId']);
-
-            // Pass timesheet data to renderer
-            $timesheetSummaryRenderView = new TimesheetSummaryRenderView($timesheetData);
-            $timesheetSummaryRenderView->render();
-            break;
-        default :
-            echo 'user is neither a submitter, nor admin.' . __FILE__;
-
-    }
+//    // Get timesheet data for user
+//    $timesheetData = new Timesheet();
+//
+//
+//    switch ($_SESSION['userType']) {
+//        case 'submitter' :
+//            $timesheetData = $timesheetData->getTimesheetsByUserId($_SESSION['userId']);
+//            //print_r($timesheetData);
+//            //Pass timesheet data to renderer
+//            $timesheetSummaryRenderView = new TimesheetSummaryRenderView($timesheetData);
+//            $timesheetSummaryRenderView->render();
+//            break;
+//        case 'admin' :
+//            $timesheetData = $timesheetData->getAssociatedUsersTimesheets($_SESSION['userId']);
+//
+//            // Pass timesheet data to renderer
+//            $timesheetSummaryRenderView = new TimesheetSummaryRenderView($timesheetData);
+//            $timesheetSummaryRenderView->render();
+//            break;
+//        default :
+//            echo 'user is neither a submitter, nor admin.' . __FILE__;
+//
+//    }
 
 
 ?>
